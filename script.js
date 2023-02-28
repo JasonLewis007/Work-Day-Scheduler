096d4292433b3e9f9b82bc1a21e7baf6c92dfc64
Jason Lewis <pensfan5050@gmail.com>
2023-02-28 17:08:55 -0500

    7th commit changes to html css js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Display today's day and date
 var todayDate = moment().format('dddd, MMM Do YYYY');
-
 $("#currentDay").html(todayDate);
 
 
@@ -45,7 +44,6 @@ $(document).ready(function () {
     }
 
     // Get item from local storage if any
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
